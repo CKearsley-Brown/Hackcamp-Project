dbe879241013d8ff174d356c020e0ef71a6f6904
Dyl-Kelly <d.kelly8@edu.salford.ac.uk>
2022-01-13 23:11:00 +0000

    Corrections

--- Models/User.php
+++ Models/User.php
@@ -5,19 +5,19 @@ require_once ('Models/UserDataSet.php');
 
 class User {
 
-    protected $username, $logged_in_status, $userID ;
+    protected $name, $logged_in_status, $userID ;
 
     //creates a user tracking object that can be used to store user data in $_SESSION
     public function __construct() {
 
         session_start();
-        $this->username = "No User";
+        $this->name = "No User";
         $this->userID = "0";
         $this->logged_in_status = false;
 
         if (isset($_SESSION["login"])) //Sets $_SESSION values if a user is logged in
             {
-                $this->username = $_SESSION["login"];
+                $this->name = $_SESSION["login"];
                 $this->logged_in_status = true;
                 $this->userID = $_SESSION["user_id"];
             }
@@ -31,19 +31,19 @@ class User {
         return $this->logged_in_status; // Getter Method
     }
 
-    public function getUname() {
-        return $this->username; // Getter Method
+    public function getName() {
+        return $this->name; // Getter Method
     }
 
-    public function AuthenticateUser($_username, $_password) {
+    public function AuthenticateUser($_name, $_password) {
         $users = new UserDataSet();
-        $usersDataSet = $users->checkUserCredentials($_username, $_password); // Creates a new dataset and calls the check credentials method
+        $usersDataSet = $users->checkUserCredentials($_name, $_password); // Creates a new dataset and calls the check credentials method
         //check credentials then takes the users username and password and checks if they exist in the database
         if (count($usersDataSet) > 0){
-            $_SESSION["login"] = $_username; //sets the $_SESSION constant of "login" to the username
+            $_SESSION["login"] = $_name; //sets the $_SESSION constant of "login" to the username
             $_SESSION["user_id"] = $usersDataSet[0]->getUserID(); //sets the $_SESSION constant of "user_id" to the users ID
             $this->logged_in_status = true;
-            $this->username = $_username;
+            $this->name = $_name;
             $this->userID = $usersDataSet[0]->getUserID();
             //echo "logged in";
             return true;
