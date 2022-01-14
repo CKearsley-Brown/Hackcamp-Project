7f34103fb2635e2d364b672b414f7fd856f1b6ec
DKearsleyBrown <d.kearsley-brown@edu.salford.ac.uk>
2022-01-14 13:52:41 +0000

    Adding Register Functionality for Employer

--- registeremployer.php
+++ registeremployer.php
@@ -9,11 +9,9 @@ if (isset($_POST["registerbutton"])) {
     require_once('Models/UserDataSet.php');
 
     $userDataSet = new UserDataSet();
-    $view->userDataSet = $userDataSet->fetchUserID(strtolower($_POST["employerEmail"]));
 
-    if (empty($view->usersDataSet)) {
+    if (true) {
         // Converting the inputs to lower case and hashing the password to ensure it is secure for storage in the database.
-        //insertNewEmployer($_ID, $_imagePath,$_companyName)
         $userDataSet->insertNewUser(strtolower($_POST["employerContactName"]), strtolower($_POST["employerEmail"]), strtolower($_POST["employerPhoneNumber"]), strtolower($_POST["employerPostalAddress"]), $_POST["employerPassword"]);
         $userID = $userDataSet->fetchUserID(strtolower($_POST["employerEmail"]));
         $userDataSet->insertNewEmployer($userID, "testPath", strtolower($_POST["employerCompanyName"]));
