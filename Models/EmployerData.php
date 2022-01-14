a8ca3671cddedc482885b055826bca98f896d606
Dyl-Kelly <d.kelly8@edu.salford.ac.uk>
2022-01-14 12:12:34 +0000

    Removing Duplicate code

--- Models/EmployerData.php
+++ Models/EmployerData.php
@@ -2,16 +2,11 @@
 
 class EmployerData {
     
-    protected $_employerID, $_contactName, $_email, $_phoneNumber, $_postalAddress, $_image, $_password, $_companyName ;
+    protected $_employerID, $_image, $_companyName ;
     
     public function __construct($dbRow) {
         $this->_employerID = $dbRow['id_employer'];
-        $this->_contactName = $dbRow['contact_name'];
-        $this->_email = $dbRow['email'];
-        $this->_phoneNumber = $dbRow['phone_number'];
-        $this->_postalAddress = $dbRow['postal_address'];
         $this->_image = $dbRow['image'];
-        $this->_password = $dbRow['password'];
         $this->_companyName = $dbRow['company_name'];
     }
 
@@ -26,50 +21,11 @@ class EmployerData {
     /**
      * @return mixed
      */
-    public function getContactName()
-    {
-        return $this->_contactName;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getEmail()
-    {
-        return $this->_email;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPhoneNumber()
-    {
-        return $this->_phoneNumber;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPostalAddress()
-    {
-        return $this->_postalAddress;
-    }
-
-    /**
-     * @return mixed
-     */
     public function getImage()
     {
         return $this->_image;
     }
 
-    /**
-     * @return mixed
-     */
-    public function getPassword()
-    {
-        return $this->_password;
-    }
 
     /**
      * @return mixed
--- Models/StudentData.php
+++ Models/StudentData.php
@@ -3,16 +3,12 @@
 class StudentData
 {
 
-    protected $_studentID, $_name, $_email, $_password, $_cv, $_phone;
+    protected $_studentID, $_cv;
 
     public function __construct($dbRow)
     {
         $this->_studentID = $dbRow['id_student'];
-        $this->_name = $dbRow['name'];
-        $this->_email = $dbRow['email'];
-        $this->_password = $dbRow['password'];
         $this->_cv = $dbRow['cv'];
-        $this->_phone = $dbRow['phone'];
     }
 
     public function getStudentID()
@@ -20,31 +16,12 @@ class StudentData
         return $this->_studentID; //getter method
     }
 
-    public function getName()
-    {
-        return $this->_name; //getter method
-    }
-
-    public function getEmail()
-    {
-        return $this->_email; //getter method
-    }
-
-    public function getPassword()
-    {
-        return $this->_password; //getter method
-    }
-
-
     public function getCV()
     {
         return $this->_cv; //getter method
     }
 
-    public function getPhone()
-    {
-        return $this->_phone;
-    }
+
 
 
 }
