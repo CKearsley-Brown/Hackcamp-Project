e7fee0dd11d2248a233eab583760da161647c0c3
Dyl-Kelly <d.kelly8@edu.salford.ac.uk>
2022-01-15 16:12:09 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,20 +4,16 @@ $view = new stdClass();
 $view->pageTitle = 'Index';
 
 require_once('Models/User.php');
+require_once('Models/UserData.php');
 $user = new User();
 $_SESSION['user'] = $user;
 
-// Login button Pressed
-if (isset($_POST["loginbutton"])) {
-
-    require_once('Models/UserData.php');
-
-    // Collect username and password for checking with password_verify
-    if (isset($_POST["loginButton"]))
-    {
-        $user->AttemptLoginUser($_POST["email"],$_POST["password"]);
-    }
+// Collect username and password for checking with password_verify
+if (isset($_POST["loginbutton"]))
+{
+    $user->AttemptLoginUser($_POST["email"],$_POST["password"]);
 }
+
 
 require_once('controller.php');
 require_once('Views/index.phtml');
