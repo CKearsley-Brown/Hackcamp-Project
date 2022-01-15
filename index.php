4a3df718c33919360ddf863ae7eabfc94e1aacac
Dyl-Kelly <d.kelly8@edu.salford.ac.uk>
2022-01-15 16:05:32 +0000

    Update index.php

--- index.php
+++ index.php
@@ -2,20 +2,19 @@
 
 $view = new stdClass();
 $view->pageTitle = 'Index';
+$user = new User();
+$_SESSION['user'] = $user;
+require_once('Models/User.php');
 
 // Login button Pressed
 if (isset($_POST["loginbutton"])) {
 
-    require_once('Models/UserDataSet.php');
+    require_once('Models/UserData.php');
 
     // Collect username and password for checking with password_verify
-    $userDataSet = new UserDataSet();
-    $view->userDataSet = $userDataSet->checkUserCredentials($_POST["email"], $_POST["password"]);
-
-    if (!empty($view->userDataSet)) {
-        $_SESSION["login"] = $_POST["userID"];
-    } else {
-        $_SESSION["error"] = "userID or Password is incorrect.";
+    if (isset($_POST["loginButton"]))
+    {
+        $user->AttemptLoginUser($_POST["email"],$_POST["password"]);
     }
 }
 
