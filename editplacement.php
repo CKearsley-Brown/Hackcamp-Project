9ea9b4628dae9bb2074fff7554ef68983ce658ac
DKearsleyBrown <97604238+DKearsleyBrown@users.noreply.github.com>
2022-01-26 12:25:45 +0000

    Added edit placement

--- editplacement.php
+++ editplacement.php
@@ -7,12 +7,28 @@ $view->pageTitle = 'Create Placement';
 unset($_SESSION["placementError"]);
 
 if ($_SESSION['user']->getLogStatus()) {
+    if ($userTableDataSet->checkIfEmployer($_SESSION['user_id'])) {
+        require_once('Models/PlacementDataSet.php');
+        $emptyPlacementDataSet = new PlacementDataSet();
+        $view->emptyPlacementDataSet = $emptyPlacementDataSet;
+        $view->placementDataSet = $emptyPlacementDataSet->employerViewAllPlacements($_SESSION['user_id']);
+    }
+
+    if (isset($_POST['placementSelectButton']))
+    {
+        if ($_POST["placementSelected"] == "noPlacement") {
+            unset($_SESSION["placementSelect"]);
+        } else {
+            $_SESSION["placementSelect"] = $_POST["placementSelected"];
+        }
+        header("Refresh:0");
+    }
+
     if (isset($_POST["editPlacementButton"])) {
         require_once('Models/PlacementDataSet.php');
 
         $placementDataSet = new PlacementDataSet();
-
-        $placementDataSet->editPlacement($_POST["placementDescripton"], $_POST["placementSkillsRequired"], $_POST["placementSalary"], $_POST["placementLocation"], $_POST["placementStart"], $_POST["placementEnd"], $_SESSION['user_id'], $_POST["placementType"]);
+        $placementDataSet->editPlacement($_SESSION['placementSelect'],$_POST["placementDescripton"], $_POST["placementSkillsRequired"], $_POST["placementSalary"], $_POST["placementLocation"], $_POST["placementStart"], $_POST["placementEnd"], $_POST["placementType"]);
     }
 }
 else
